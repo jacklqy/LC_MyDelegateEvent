aa1c6aab56a64b38f97e449274789dca239c4a1e
探lu者 <529987528@qq.com>
2021-04-07 11:13:48 +0800

    init LC_MyDelegateEvent

---
+++ LC_MyDelegateEvent/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LC_MyDelegateEvent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
